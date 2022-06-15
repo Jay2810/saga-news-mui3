73ee9bf5294dae3bc46a8996d0e0edadbf916ac5
Jay2810 <pateljay1628@gmail.com>
2022-06-15 13:26:14 +0530

    MUI

---
+++ src/containers/newscard.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+import { connect } from "react-redux";
+
+export default function MultiActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+const mapStateToProps = (state) => ({
+  news: state.news,
+  loading: state.loading,
+});
+
+MultiActionAreaCard = connect(mapStateToProps, null)(MultiActionAreaCard);
